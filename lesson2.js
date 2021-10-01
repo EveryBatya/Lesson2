8f08cd2f98cbe27914fe45c199e4e309fcfe0c2f
EveryBatya <pavelnekrasov961@gmail.com>
2021-10-01 17:16:30 +0500

    Сделал дз по второму уроку

---
+++ lesson2.js
@@ -0,0 +1,18 @@
+let title = "Lesson 2";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1234;
+let rollback = 48;
+let fullPrice = 34920;
+let adaptive = true;
+
+console.log(typeof title, typeof screens, typeof screenPrice);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей. 
+            Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase)
+
+
+
+
+
+

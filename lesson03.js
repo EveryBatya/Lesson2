6e5571d20443d5dc59e6ac7dc353df3348f7a3e6
EveryBatya <34242079+EveryBatya@users.noreply.github.com>
2021-10-13 02:25:46 +0500

    дз 8 урока

--- lesson03.js
+++ lesson03.js
@@ -2,11 +2,10 @@
 
 const appData = {
     title: '',
-    screens: '',
+    screens: [],
     screenPrice: 0,
     adaptive: true,
-    service1: '',
-    service2: '',
+    services: {},
     servicePrice: 0,
     fullPrice: 0,
     servicePercentPrice: 0, 
@@ -15,64 +14,89 @@ const appData = {
 
     start: function() {
         this.asking();
-        this.allServicePrices = this.getAllServicePrices([this.service1, this.service2]);
-        this.fullPrice = this.getFullPrice();
-        this.servicePercentPrice = this.getServicePercentPrices();
-        this.title = this.getTitle(); 
+        this.addPrices();
+        this.getFullPrice();
+        this.getServicePercentPrices();
+        this.getTitle(); 
+
         this.logger();
     },
 
     logger: function() {
+        console.log(this.getRollBack(this.fullPrice));
         for (let key in appData) console.log(key, appData[key]);
     },
 
     asking: function() {
-        this.title = prompt('Как называется твой проект?', 'Свартальхейм');
-        this.screens = prompt('Какие типы экранов нужно разработать?', 'Сложные');
-        this.screenPrice = prompt('Сколько будет стоить данная работа?');
+
+        this.title = this.getStrAnswer(this.title, 'Как называется твой проект?');
+        console.log(this.title);
     
-        while(!this.isNumber(this.screenPrice)) {
-            this.screenPrice = prompt('Сколько будет стоить данная работа?');
+        for (let i = 0; i < 2; i++) {
+            let name;
+            name = this.getStrAnswer(name, 'Какие типы экранов нужно разработать?');
+
+            let price;
+            price = this.getNumAnswer(price, 'Сколько это будет стоить?');
+
+            this.screens.push({id: i, name: name, price: price});
+        }
+
+        for (let i = 0; i < 2; i++) {
+            let name;
+            name = this.getStrAnswer(name, 'Какой дополнительный тип услуги нужен?');
+
+            let price;
+            price = this.getNumAnswer(price, 'Сколько это будет стоить?');
+            
+            this.services[name] = +price;
         }
     
         this.adaptive = confirm('Нужен ли адаптив на сайте?');
         
     },
 
+    addPrices: function() {
+        for (let screen of this.screens) this.screenPrice += +screen.price;
+        for (let key in this.services) this.allServicePrices = this.services[key];
+    },
+ 
     isNumber: function(num) {
         return !isNaN(parseFloat(num)) && isFinite(num);
     },
 
-    getAllServicePrices: function(arr) {
-        let sum = 0;
-    
-        for (let i = 0; i < arr.length; i++) {
-            arr[i] = prompt('Какой дополнительный тип услуги нужен?', 'Метрика');
-            this.servicePrice = prompt('Сколько это будет стоить?');
-            while(!this.isNumber(this.servicePrice)) this.servicePrice = prompt('Сколько это будет стоить?');
-            sum += +this.servicePrice.trim();
-        }
-        return +sum;
+    getNumAnswer: function(answer, str) {
+        do {
+            answer = prompt(str);
+        } while (!this.isNumber(answer));
+        return answer;
+    },
+
+    getStrAnswer: function(answer, str) {
+        do {
+            answer = prompt(str);
+        } while (this.isNumber(answer));
+        return answer;
     },
  
-   getFullPrice: function () {
-        return +this.allServicePrices + +this.screenPrice;
+    getFullPrice: function() {
+        this.fullPrice = +this.allServicePrices + +this.screenPrice;
     },
     
-   getTitle: function () {
+   getTitle: function() {
         let count = 0;
         for (let i = 0; i < this.title.length; i++) {
             if (this.title[i] == ' ') count++;
             else break; 
         }
-        return this.title[count].toUpperCase() + this.title.slice(count + 1).toLowerCase();
+        this.title = this.title[count].toUpperCase() + this.title.slice(count + 1).toLowerCase();
     },
     
-   getServicePercentPrices: function () { 
-        return +this.fullPrice - (+this.fullPrice * (+this.rollback / 100));
+   getServicePercentPrices: function() { 
+        this.servicePercentPrice = +this.fullPrice - (+this.fullPrice * (+this.rollback / 100));
     },
     
-   getRollBack: function (price) {
+   getRollBack: function(price) {
         if (price >= 30000) return ('Даем скидку 10%');
         else if (price <= 30000 && price >= 15000) return ('Даем скидку 5%');
         else if (price <= 15000 && price >= 0) return ('Скидка не предусмотрена');
